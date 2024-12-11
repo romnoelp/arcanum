bd19642a0edbe017682e2a26d18d5203d8b53cb6
romnoelp <petracorta.rmnl@gmail.com>
2024-12-12 03:10:30 +0800

    Nothing to freaking lsoe part 8

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -44,7 +44,7 @@ export async function updateSession(request: NextRequest) {
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
-    url.pathname = "/admin";
+    url.pathname = "https://arcanum-two.vercel.app/admin";
     return NextResponse.redirect(url);
   }
 
