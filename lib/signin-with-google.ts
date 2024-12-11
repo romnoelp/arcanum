bbad9b2ae7a6d028aa60c06681e8b9b164da4633
romnoelp <petracorta.rmnl@gmail.com>
2024-12-12 01:11:06 +0800

    Added a getBaseURL function.

--- lib/signin-with-google.ts
+++ lib/signin-with-google.ts
@@ -1,11 +1,23 @@
 import { redirect } from "next/navigation";
 import { createClient } from "../utils/supabase/client";
 
+function getBaseURL() {
+  if (typeof window !== "undefined") return "";
+
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+
+  if (process.env.RENDER_INTERNAL_HOSTNAME)
+    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
+
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
 export const signInWithGoogle = async () => {
   const supabase = createClient();
   const { error: authError } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
+      redirectTo: `${getBaseURL()}/auth/callback`,
       queryParams: {
         access_type: "offline",
         prompt: "consent",
